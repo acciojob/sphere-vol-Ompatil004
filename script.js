2c9b15e57d19ae73210f4029578da185353dc170
Om Patil <155781720+Ompatil004@users.noreply.github.com>
2026-01-22 13:35:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
+
 function volume_sphere() {
-    //Write your code here
-  
+	 event.preventDefault(); // stop form reload
+
+    let radius = document.getElementById("radius").value;
+    let volume = document.getElementById("volume");
+
+    let vol = (4 / 3) * Math.PI * Math.pow(radius, 3);
+
+    volume.value = vol;
+
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
